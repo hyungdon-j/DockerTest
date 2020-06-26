e26735bc511663ab0627c13b8ae26bb217fb1020
hyungdon <joojoo0513@gmail.com>
2020-06-26 17:12:43 +0900

    final

--- k8s/k8s.sh
+++ k8s/k8s.sh
@@ -1,1 +1,4 @@
-kubectl expose deployment/mynginx --type="NodePort" --port 80
+
+kubectl run $1 --image=hyungdon/nginx_ubuntu --port=80
+kubectl scale  deployment $1 --replicas=20
+kubectl expose deployment/$1 --type="NodePort" --port 80
